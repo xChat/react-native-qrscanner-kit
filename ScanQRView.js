54ea3e30d1a5d16ed6e36b3dfa02bb0968690afc
Jerry <jerry.li.love@gmail.com>
2016-12-08 12:27:54 +1100

    Update ScanQRView.js

--- ScanQRView.js
+++ ScanQRView.js
@@ -7,34 +7,34 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View , 
-  Dimensions , 
-  Vibration , 
-  Animated , 
-  Easing , 
-  Alert 
+  View ,
+  Dimensions ,
+  Vibration ,
+  Animated ,
+  Easing ,
+  Alert ,
+  TouchableOpacity
 } from 'react-native';
 import Camera from 'react-native-camera';
 export default class ScanQRView extends Component {
-  
+
   constructor(props){
-      super(props) ;
+    super(props);
     this.state={
-      scanTranslte:new Animated.Value(0) 
-    }
+      scanTranslte:new Animated.Value(0)
+    };
   }
   componentDidMount(){
-    
     this.scanLineAnimation();
   }
   scanLineAnimation(){
     Animated.sequence([
       Animated.timing(this.state.scanTranslte,{toValue:200 , duration:3000 , easing:Easing.linear}),
       Animated.timing(this.state.scanTranslte,{toValue:0 , duration:3000 , easing:Easing.linear})
-                       ]).start(()=>this.scanLineAnimation()) ;
+                       ]).start(()=>this.scanLineAnimation());
   }
   componentWillUnmout(){
-    
+
   }
   onBarCodeRead(result){
     Vibration.vibrate();
@@ -43,25 +43,30 @@ export default class ScanQRView extends Component {
         this.props.onBarCodeRead(result , ()=>this.camera.shouldQR());
     }
     else
-      Alert.alert( "扫描结果" , result.data , [{text:"ok" , onPress:()=>this.camera.shouldQR()}] , {cancelable:false}) ; 
-    
+      Alert.alert( "扫描结果" , result.data , [{text:"ok" , onPress:()=>this.camera.shouldQR()}] , {cancelable:false});
+  }
+  onCancel(){
+    if(this.props.cancel)
+    {
+      this.props.cancel();
+    }
   }
   render() {
-    let width = Dimensions.get('window').width ; 
-    let height = Dimensions.get('window').height ;
-    let scanCoverColor = '#44444488' ;
-    let scanRectWidth = 200 ;
-    console.log("render size = " + width +" height = " + height ) ;
+    let width = Dimensions.get('window').width;
+    let height = Dimensions.get('window').height;
+    let scanCoverColor = '#44444488';
+    let scanRectWidth = 200;
+    // console.log("render size = " + width +" height = " + height );
     return (
       <View style={{flex:1}}>
-        <Camera 
+        <Camera
           style={{width:width , height:height}}
           type={Camera.constants.Type.back}
           ref={(cam) => {
             this.camera = cam;
           }}
           onBarCodeRead={this.onBarCodeRead.bind(this)}
-          /> 
+          />
         <View
           style={{position:'absolute' , left:0 , right:0, top:0 , bottom:0}}
           >
@@ -69,12 +74,19 @@ export default class ScanQRView extends Component {
           <View style={{width:width , height:scanRectWidth ,flexDirection:'row'}}>
             <View style={{flex:1,backgroundColor:scanCoverColor}}/>
             <View style={{width:scanRectWidth}}>
-              <Animated.View style={{width:scanRectWidth , height:2 , backgroundColor:"#fe2233" , transform:[
-                                     {translateY:this.state.scanTranslte}
-                                   ] }}/>
+              <Animated.View
+                style={{width:scanRectWidth , height:2 , backgroundColor:"#fe2233" , transform:[
+                    {translateY:this.state.scanTranslte}
+                  ]
+                }}
+               />
+               <View style={{ flex:1, marginTop: scanRectWidth-30, backgroundColor:'transparent', alignItems: 'center'}}>
+                 <TouchableOpacity onPress={this.onCancel.bind(this)} activeOpacity={0.9}>
+                   <Text style={{color:'#fff'}}>{this.props.backButtonText}</Text>
+                 </TouchableOpacity>
+               </View>
             </View>
             <View style={{flex:1 , backgroundColor:scanCoverColor}}/>
-            
           </View>
           <View style={{flex:1 , width:width , backgroundColor:scanCoverColor}}/>
         </View>
@@ -82,4 +94,3 @@ export default class ScanQRView extends Component {
     );
   }
 }
-
